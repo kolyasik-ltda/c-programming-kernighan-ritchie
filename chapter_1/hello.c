ee6d22730c4315e236d5506b8d736288b8adbd08
Nicolas Lopes <kolyasik.telefone@gmail.com>
2024-12-25 17:55:39 -0300

    Add files via upload

---
+++ chapter_1/hello.c
@@ -0,0 +1,7 @@
+#include <stdio.h>              // include information about standard library
+                                
+int main()                      // define a function named main that receives no argument values
+{                               // statment of main are enclosed in braces
+    printf("Hello, World\n");   // main calls library function printf to print this sequence of
+    return 0;                   // characters; \n represent the newline character
+}
